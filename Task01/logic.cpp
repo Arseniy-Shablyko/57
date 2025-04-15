7fd1c28f7d58fb1580fe4feb3189b0fa4a9fc0e1
Arseniy Shablyko <241208arseniy@gmail.com>
2025-04-15 17:42:50 +0300

    Add project files.

---
+++ Task01/logic.cpp
@@ -0,0 +1,26 @@
+﻿//	[The Count of Even Digits]
+//	Количество чётных цифр 
+//
+//	Необходимо разработать рекурсивную функцию, которая подсчитывает 
+//	количество чётных цифр у заданного пользователем целого числа.
+//
+//	Цифра ноль в математике является чётным числом. 
+//
+//	Число может быть как положительным, так и отрицательным.
+
+#include "logic.h"
+
+int count_even_digits(long long number) {
+	/*if (number < 0) {
+		number *= (-1);
+	}*/
+
+	if (number < 10 && number > -10) {
+		return number % 2 == 0 ? 1 : 0;
+	}
+
+	int last_number = number % 10;
+	last_number = last_number % 2 == 0 ? 1 : 0;
+
+	return last_number + count_even_digits(number / 10);
+}
